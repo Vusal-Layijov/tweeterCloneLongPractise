c3541f066f1b337916749ab4ac9871b4ce52dbd6
Vusal Layijov <Layijovvusal@gmail.com>
2023-02-28 13:28:14 -0800

    phase 2 completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 # !!START
-from flask import Flask
+from flask import Flask,render_template
 from .config import Config
+from .tweets import tweets
+from random import randint
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 # !!END
+@app.route('/')
+def home():
+    randInt=randint(0,4)
+    tweet=tweets[randInt]
+    return render_template('index.html', tweet=tweet)
+
+@app.route('/feed')
+def feed():
+    return render_template('feed.html', tweets=tweets)
