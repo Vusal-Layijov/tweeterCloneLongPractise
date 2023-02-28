0b22549d8f9b9708b3c279d2734c78b29f781b1b
Vusal Layijov <Layijovvusal@gmail.com>
2023-02-28 15:07:38 -0800

    last Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 # !!START
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect
 from .config import Config
 from .tweets import tweets
 from .form.form import CreateTweet
 from random import randint
+from datetime import date
 
 app = Flask(__name__)
 
@@ -17,9 +18,25 @@ def home():
 
 @app.route('/feed')
 def feed():
-    return render_template('feed.html', tweets=tweets)
+    sortedTweets = sorted(tweets, key=lambda tweet: tweet['date'], reverse=True)
+    return render_template('feed.html', tweets=sortedTweets)
 
-@app.route('/new')
+@app.route('/new', methods=["GET","POST"])
 def create_tweet():
+
     form=CreateTweet()
-    return render_template('new_tweet.html', form=form)
+    if form.validate_on_submit():
+        new_tweet={
+            'id':len(tweets)+1,
+            "author":form.data['author'],
+            'tweet':form.data['tweet'],
+            'likes':0,
+            "date": str(date.today().strftime("%d/%m/%Y"))
+        }
+        print(new_tweet)
+        tweets.append(new_tweet)
+        return redirect('/feed')
+    if form.errors:
+        print(form.errors)
+        return render_template('new_tweet.html', form=form, errors=form.errors)
+    return render_template('new_tweet.html', form=form, errors=None)
